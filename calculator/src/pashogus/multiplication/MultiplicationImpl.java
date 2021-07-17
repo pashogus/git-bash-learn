5159ba97d92a1f817b6863c22f4caacdec0ef13a
Shanmugasundaram Palaninathan <shanmugasundaram.p2@cognizant.com>
2021-07-17 23:50:05 +0530

    Adding new Functionality for Multiplication of Two Integers

---
+++ calculator/src/pashogus/multiplication/MultiplicationImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pashogus.multiplication;
+
+/**
+ * @author 540091
+ *
+ */
+public class MultiplicationImpl implements Multipliction {
+
+	/* (non-Javadoc)
+	 * @see pashogus.multiplication.Multipliction#multiply(int, int)
+	 */
+	@Override
+	public int multiply(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ calculator/src/pashogus/multiplication/Multipliction.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package pashogus.multiplication;
+
+/**
+ * @author 540091
+ *
+ */
+public interface Multipliction {
+
+	int multiply(int a , int b);
+}
