bfd64701814d00d387d9cdec2a3aa7a0461f34f3
Shanmugasundaram Palaninathan <shanmugasundaram.p2@cognizant.com>
2021-07-17 23:35:33 +0530

    calculator for two integers with addition and Subraction

--- calculator/src/pashogus/main/Main.java
+++ calculator/src/pashogus/main/Main.java
@@ -1,10 +1,21 @@
 package pashogus.main;
 
+import pashogus.addition.Addition;
+import pashogus.addition.AdditionImpl;
+import pashogus.subraction.Subraction;
+import pashogus.subraction.SubractionImpl;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Addition addition = new AdditionImpl();
+		System.out.println(addition.add(3, 5));
+		
+		Subraction subraction = new SubractionImpl();
+		System.out.println(subraction.sub(5, 3));
+		
+		
 	}
 
 }
