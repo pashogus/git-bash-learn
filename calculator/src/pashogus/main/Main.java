80c5f7cc4d90c20f5bafc8ef96908c8046578c3e
Shanmugasundaram Palaninathan <shanmugasundaram.p2@cognizant.com>
2021-07-17 23:53:26 +0530

    updated Changes for addition of two numbers after Merging with Master

--- calculator/src/pashogus/main/Main.java
+++ calculator/src/pashogus/main/Main.java
@@ -1,26 +1,12 @@
 package pashogus.main;
 
-import pashogus.addition.Addition;
-import pashogus.addition.AdditionImpl;
-import pashogus.multiplication.MultiplicationImpl;
-import pashogus.multiplication.Multipliction;
-import pashogus.subraction.Subraction;
-import pashogus.subraction.SubractionImpl;
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		Addition addition = new AdditionImpl();
-		System.out.println(addition.add(3, 5));
-		
-		Subraction subraction = new SubractionImpl();
-		System.out.println(subraction.sub(5, 3));
-		
-		Multipliction multiplication = new MultiplicationImpl();
-		System.out.println(multiplication.multiply(2, 3));
 		
 		
+
 	}
 
 }
