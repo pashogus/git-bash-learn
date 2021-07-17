d7d9cbbe09f0c542fe215ccafac60f21e01de0c2
Shanmugasundaram Palaninathan <shanmugasundaram.p2@cognizant.com>
2021-07-18 00:29:06 +0530

    additional functionality with multilication of two integers

--- calculator/src/pashogus/main/Main.java
+++ calculator/src/pashogus/main/Main.java
@@ -2,6 +2,8 @@ package pashogus.main;
 
 import pashogus.addition.Addition;
 import pashogus.addition.AdditionImpl;
+import pashogus.multiplication.MultiplicationImpl;
+import pashogus.multiplication.Multipliction;
 import pashogus.subraction.Subraction;
 import pashogus.subraction.SubractionImpl;
 
@@ -14,6 +16,9 @@ public class Main {
 		
 		Subraction subraction = new SubractionImpl();
 		System.out.println(subraction.sub(5, 3));
+		
+		Multipliction multiplication = new MultiplicationImpl();
+		System.out.println(multiplication.multiply(2, 3));
 		
 		
 	}
